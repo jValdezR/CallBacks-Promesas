a50d075673e56a3cd75522d9b10ecf4e13b92a06
Jesus Valdez <jdv_rodriguez@hotmail.com>
2020-08-21 10:06:09 -0500

    CallBack Hell

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import {buscarHeroe} from './js/callbacks'
 
 const heroeId = 'ironman';
 
-buscarHeroe(heroeId,(heroe) =>{
-    
-    if(heroe){
-        console.log(heroe);
-    }
-    else{
-        console.error(Error)
-    }
-    
+//CallBack Hell
+buscarHeroe(heroeId, (err, heroe) => {
+
+    if (err) { return console.error(err); }
+
+    buscarHeroe(heroeId2, (err, heroe2) => {
+        if (err) { return console.error(err); }
+
+        buscarHeroe(heroeId3, (err, heroe3) => {
+            if (err) { return console.error(err); }
+
+            console.log(`Enviando a ${heroe.nombre} y a ${heroe2.nombre} y a ${heroe3.nombre}`);
+        });
+    });
 });
